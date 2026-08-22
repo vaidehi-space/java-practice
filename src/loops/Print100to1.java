d1f531f12cd10fe62eb3e5262123aac7fed72876
Vaidehi Kolhe <vaidehikolhe07@gmail.com>
2026-08-23 00:47:52 +0530

    Add Print 1 to 100 and 100 to 1 basic loops programs in Java.

---
+++ src/loops/Print100to1.java
@@ -0,0 +1,11 @@
+package loops;
+
+//Print numbers from 100 to 1
+public class Print100to1{
+    public static void main(String[]args){
+        int i;
+        for(i=100;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/loops/Print1to100.java
@@ -0,0 +1,11 @@
+package loops;
+
+//Print numbers 1 to 100
+public class Print1to100 {
+    public static void main(String[] args){
+        int i;
+        for(i=1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
