dc07c2b2af3ed69228763d7ed1a422f32c7dc191
Vaidehi Kolhe <vaidehikolhe07@gmail.com>
2026-08-24 01:09:25 +0530

    Add (1) Print Even numbers from 1 to 100 and (2) Print Odd numbers 1 to 100 basic loop programs in Java.

---
+++ src/loops/PrintEven1to100.java
@@ -0,0 +1,10 @@
+package loops;
+
+//Print even numbers from 1 to 100
+public class PrintEven1to100 {
+    public static void main(String[]args){
+        for(int i=0; i<=100; i+=2){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/loops/PrintOdd1to100.java
@@ -0,0 +1,10 @@
+package loops;
+
+//Print odd numbers from 1 to 100
+public class PrintOdd1to100 {
+    public static void main(String[]args){
+        for(int i=1; i<=100; i+=2){
+            System.out.println(i);
+        }
+    }
+}
